9264272cca688510c568c02ab3c5b00f8bb25fb4
vakson-cpu <vakson12@gmail.com>
2023-04-21 01:54:52 +0200

    Create screens

---
+++ Screens/Authentication.js
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native"
+const Authentication = () => {
+  return (
+    <View style = {{display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <Text>Authenication</Text>
+    </View>
+  )
+}
+
+export default Authentication
---
+++ Screens/Home.js
@@ -0,0 +1,28 @@
+import { View, Text, Button } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// const Home = ({ navigation }) => {
+
+
+//   const handerOnPress = () => {
+//     navigation.navigate("LeaderBoard");
+//   };
+
+//   return (
+//       <View>
+//         <Text>Home</Text>
+//         <Button title="Go to leaderboard" onPress={handerOnPress} />
+//       </View>
+//   );
+// };
+
+// export default Home;
+
+const Home = () => {
+  return (
+    <Text>
+      Hello
+    </Text>
+  )
+}
+
+export default Home
